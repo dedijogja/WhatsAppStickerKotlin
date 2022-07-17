bc426c10b70fb970ea477e4978e4028bf77452b5
dedijogja <dedistyawan68@yahoo.com>
2022-07-17 19:53:20 +0700

    use android jetpack ViewBinding

--- app/src/main/java/com/dedijogja/whatsappstickerkotlin/view/SplashActivity.kt
+++ app/src/main/java/com/dedijogja/whatsappstickerkotlin/view/SplashActivity.kt
@@ -2,23 +2,30 @@ package com.dedijogja.whatsappstickerkotlin.view
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import com.dedijogja.whatsappstickerkotlin.R
+import androidx.appcompat.app.AppCompatActivity
+import com.dedijogja.whatsappstickerkotlin.databinding.ActivitySplashBinding
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        bukaMainActivity()
+
+    }
+
+    private fun bukaMainActivity(){
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }, 3000)
-
     }
 
     override fun onBackPressed() {
