7f9026e784153dce4b937c0ebbb49668aa2efea4
dedijogja <dedistyawan68@yahoo.com>
2022-07-17 18:55:48 +0700

    menambah SplashActivity dan merubah Icon

--- app/src/main/java/com/dedijogja/whatsappstickerkotlin/view/MainActivity.kt
+++ app/src/main/java/com/dedijogja/whatsappstickerkotlin/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.dedijogja.whatsappstickerkotlin
+package com.dedijogja.whatsappstickerkotlin.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dedijogja.whatsappstickerkotlin.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/dedijogja/whatsappstickerkotlin/view/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.dedijogja.whatsappstickerkotlin.view
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.dedijogja.whatsappstickerkotlin.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+
+    override fun onBackPressed() {
+        //override onbackpressed agar user tidak dapat menutup splash screen
+    }
+}
